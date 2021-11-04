b32de3ce045bd4d84ec8fa02c906b88fc72860a4
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-04 15:36:06 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>| Home | Laravel Primi passi!</title>
+</head>
+<body>
+    <h1>
+        Laravel - Primi passi
+    </h1>
+    <h3>
+        Hello {{$user["name"]}}!
+    </h3>
+    <h5>
+        Check your email {{$user["email"]}} for more information! 
+    </h5>
+    <h6>
+        Lie, it's a joke. Shhhh @for($i = 0; $i < 3; $i++).@endfor 
+    </h6>
+</body>
+</html>
